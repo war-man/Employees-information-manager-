a1693ddda24c892517273ad96a7efde33c630fed
Kallaf <kalaf1997@yahoo.com>
2020-06-04 18:27:14 +0200

    Seed Skill table with some skills

---
+++ Models/SeedData.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using EmployeesInformationManager.Data;
+using System;
+using System.Linq;
+
+namespace EmployeesInformationManager.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new EmployeesInformationManagerContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<EmployeesInformationManagerContext>>()))
+            {
+                // Look for any movies.
+                if (context.Skill.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Skill.AddRange(
+                    new Skill {Name = "PHP"},
+                    new Skill {Name = "ASP.NET"},
+                    new Skill {Name = "iOS"},
+                    new Skill {Name = "Android"}
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
