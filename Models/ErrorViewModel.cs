58dc251cb0ab0338637baacbd009ef6572d4922d
Kallaf <kalaf1997@yahoo.com>
2020-06-04 12:50:21 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Employees_information_manager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
